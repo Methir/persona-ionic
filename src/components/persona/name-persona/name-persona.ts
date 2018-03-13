e72c94cb45d8d427cefb14f86e85f94b67354ffb
Gabriel Nascimento <gabriel.mfn@gmail.com>
2018-03-13 17:23:34 -0300

    Implementado componente name-persona

--- src/components/persona/name-persona/name-persona.ts
+++ src/components/persona/name-persona/name-persona.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, forwardRef } from '@angular/core';
 
-/**
- * Generated class for the NamePersonaComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => NamePersonaComponent),
+  multi: true
+};
+
 @Component({
   selector: 'name-persona',
-  templateUrl: 'name-persona.html'
+  templateUrl: 'name-persona.html',
+  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
 })
-export class NamePersonaComponent {
+export class NamePersonaComponent implements ControlValueAccessor{
 
-  text: string;
+  private onChange;
+  private value: string;
 
   constructor() {
     console.log('Hello NamePersonaComponent Component');
-    this.text = 'Hello World';
   }
 
+  get nameValue(): string {
+    return this.value;
+  }
+
+  set nameValue(value) {
+    this.value = value;
+    this.onChange(value);
+  }
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void { }
+  setDisabledState?(isDisabled: boolean): void { }
 }
